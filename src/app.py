6645321a6e97e5c96b8280207d394ebba4f64392
Otávio Augusto <otavioaugustobrb19@gmail.com>
2023-06-30 17:19:16 -0300

    fix: get dummies problem fixed

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from sklearn.neighbors import KNeighborsClassifier
 import pickle
 import pandas as pd
 
@@ -29,20 +30,14 @@ def predict():
     goout = int(request.form['goout'])
     dalc = int(request.form['dalc'])
     walc = int(request.form['walc'])
-    absences = int(request.form['abseneces'])
+    absences = int(request.form['absences'])
     g3 = int(request.form['g3'])
 
     # Create a DataFrame with the input data
     input_data = pd.DataFrame({
         'age': [age],
-        'Pstatus': [parent_status],
-        'Mjob': [mother_job],
-        'Fjob': [father_job],
-        'guardian': [guardian],
         'studytime': [study_time],
         'failures': [failures],
-        'internet': [internet],
-        'romantic': [romantic],
         'famrel': [famrel],
         'freetime': [freetime],
         'goout': [goout],
@@ -52,12 +47,34 @@ def predict():
         'G3': [g3]
     })
 
+    not_int_input = pd.DataFrame({
+        'Pstatus': [parent_status],
+        'Mjob': [mother_job],
+        'Fjob': [father_job],
+        'guardian': [guardian],
+        'internet': [internet],
+        'romantic': [romantic],
+    })
 
-    # Perform one-hot encoding on the input data
-    input_data_encoded = pd.get_dummies(input_data)
+    input_data_encoded = pd.get_dummies(data=not_int_input)
+
+    expected_cols = ['age', 'studytime', 'failures', 'famrel', 'freetime', 'goout', 'Dalc',
+       'Walc', 'absences', 'G3', 'Pstatus_A', 'Pstatus_T', 'Mjob_at_home',
+       'Mjob_health', 'Mjob_other', 'Mjob_services', 'Mjob_teacher',
+       'Fjob_at_home', 'Fjob_health', 'Fjob_other', 'Fjob_services',
+       'Fjob_teacher', 'guardian_father', 'guardian_mother', 'guardian_other',
+       'internet_no', 'internet_yes', 'romantic_no', 'romantic_yes']
+    
+    input_data_final = pd.concat([input_data, input_data_encoded], axis=1)
+
+    missing_cols = set(expected_cols) - set(input_data_final.columns)
+    for col in missing_cols:
+        input_data_final[col] = 0
+
+    input_data_final = input_data_final[expected_cols]
 
     # Make predictions using the loaded model
-    prediction = knn_model.predict(input_data_encoded)
+    prediction = knn_model.predict(input_data_final)
 
     # Pass the prediction result to the template
     return render_template('result.html', prediction=prediction[0])
